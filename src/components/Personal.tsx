3d6f3e570ef791d06677ff4b619847182a4e6835
EyadOmar <eiad.omar88@gmail.com>
2025-03-03 23:20:28 +0200

    3/3/2025 update

--- src/components/Personal.tsx
+++ src/components/Personal.tsx
@@ -7,30 +7,13 @@ function Personal() {
         Hello, I am Eyad Omar
       </h1>
       <p className=" text-pretty text-white/50 md:text-lg lg:text-xl">
-        I'm Eyad Omar, a front-end developer mainly using react, living in Egypt
-        with three years of experience. Currently working full-time as a
-        computer science teacher assistant at{" "}
-        <a
-          className="text-foreground-300 font-medium underline transition-colors duration-200 ease-in-out hover:text-white"
-          href="https://www.mti.edu.eg/faculty/2/computing"
-          target="_blank"
-        >
-          MTI University
-        </a>{" "}
-        and part-time as a front-end developer at{" "}
-        <a
-          className="text-foreground-300 font-medium underline transition-colors duration-200 ease-in-out hover:text-white"
-          href="https://www.linkyou.ca/"
-          target="_blank"
-        >
-          LinkYou Marketing
-        </a>
-        .
+        A full stack developer that has created numerous apps and is eager to
+        create more.
       </p>
       <a
         href="cv.pdf"
         download={true}
-        className="bg-accent mt-6 block w-fit rounded-sm px-4 py-2  font-bold transition-all duration-200 ease-in-out hover:scale-105 hover:brightness-110 md:text-xl"
+        className="mt-6 block w-fit rounded-sm bg-accent px-4 py-2  font-bold transition-all duration-200 ease-in-out hover:scale-105 hover:brightness-110 md:text-xl"
       >
         Download CV
       </a>
--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -2,22 +2,22 @@ const tools = [
   {
     id: "skill_1",
     title: "HTML & CSS & JS",
-    desc: "My main tools, I can say I know them very well and I am still learning them everyday and everyhour",
+    desc: "Fundamentalssss...",
   },
   {
     id: "skill_2",
     title: "React",
-    desc: "My top and only library to manage front-end logic, I really like the component based approch, its file structure, and the community",
+    desc: "My top and only library to manage front-end logic, I really like the component based approch, its file structure, and the community. I use it alone with vite only when I am building SPAs like this website but most of the time I use it in framework like Nextjs",
   },
   {
     id: "skill_3",
     title: "Typescript",
-    desc: "I upgraded my javascripts with the power of types. It's usefull, organized, and more professional",
+    desc: "I upgraded my javascripts with the power of types. It's useful, organized, and more professional",
   },
   {
     id: "skill_4",
     title: "TailwindCSS",
-    desc: "Its very compatiable with React, and I like it more than bootstrap but I can still use bootstrap easily",
+    desc: "Its very compatible with React, and I like it more than bootstrap but I can still use bootstrap easily",
   },
   {
     id: "skill_5",
@@ -28,6 +28,36 @@ const tools = [
     id: "skill_6",
     title: "React Query & Axios",
     desc: "My favorite tools to deal with APIs",
+  },
+  {
+    id: "skill_6",
+    title: "Nextjs",
+    desc: "My favorite react framework",
+  },
+  {
+    id: "skill_7",
+    title: "shadcn/ui",
+    desc: "My favorite UI library, I am a veteran in using it and modifying it",
+  },
+  {
+    id: "skill_8",
+    title: "Nodejs & Express",
+    desc: "my backend tongue",
+  },
+  {
+    id: "skill_9",
+    title: "Nestjs",
+    desc: "best framework for Node.js, I love it",
+  },
+  {
+    id: "skill_10",
+    title: "PostgreSQL / MongoDB",
+    desc: "my favorite relational/non-relational database. of course, I can use any database if it has documentation online I can use it but those what I work with the most",
+  },
+  {
+    id: "skill_11",
+    title: "And Many more...",
+    desc: "of course I am a veteran git and github user, I also dealt with figma, adobe xd, I also used cmd alot while dealing with VPS, and more more.",
   },
 ];
 
